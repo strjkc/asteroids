45ff59469401e672d755331c1296ad043755646f
strahinja <strahinjaa.va@gmail.com>
2025-05-19 17:52:10 +0200

    init

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+from constants import *
+
+def main():
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    loaded = pygame.init()
+    print(f"Loaded {loaded[0]} modules")
+    print(f"Failed to load {loaded[1]} modules")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen,(0,0,0))
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
