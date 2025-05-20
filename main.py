a05c1276685a3703b544051f8d10548a2085a5d4
strjkc <strahinjaa.va@gmail.com>
2025-05-20 19:13:32 +0200

    implement player

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from player import Player
 import pygame
 from constants import *
 
@@ -9,12 +10,24 @@ def main():
     print(f"Loaded {loaded[0]} modules")
     print(f"Failed to load {loaded[1]} modules")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen,(0,0,0))
+        for item in updatable:
+            item.update(dt)
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
+        rt_val = clock.tick(60)
+        dt = rt_val / 1000
 
 if __name__ == "__main__":
     main()
